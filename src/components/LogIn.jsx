c7962fe451df5739402bf758074e2af8919f0b55
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-25 22:32:37 +0600

    basic route setup done

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,9 @@
+const LogIn = () => {
+    return (
+        <div>
+            <h2>log in page</h2>
+        </div>
+    );
+};
+
+export default LogIn;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import App from './App';
+import LogIn from './components/LogIn';
+import Register from './components/Register';
+import ErrorPage from './components/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>
+  },
+  {
+    path: "/logIn",
+    element: <LogIn></LogIn>
+  },
+  {
+    path: "/register",
+    element: <Register></Register>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
