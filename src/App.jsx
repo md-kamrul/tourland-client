c2ad3cf6bdb93d244df230d78382da200be39a14
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-26 12:09:28 +0600

    navbar running

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1>Tourism Website</h1>
+      
+      {/* navbar */}
+      <Navbar></Navbar>
+      
+      <Outlet></Outlet>
+
     </>
   )
 }
