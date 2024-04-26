cd305ca87317922de33002ef0fcfb37b18ec1dca
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-26 22:43:07 +0600

    footer setup done

--- src/root/Roots.jsx
+++ src/root/Roots.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const Roots = () => {
     return (
         <div>
 
             <Outlet></Outlet>
+
+            <Footer></Footer>
             
         </div>
     );
