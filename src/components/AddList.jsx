2dd0fc56a761c641407747a6b18eabfb81a34b90
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-29 00:04:38 +0600

    add list section running

--- src/components/AddList.jsx
+++ src/components/AddList.jsx
@@ -1,10 +1,21 @@
 import Navbar from "./Navbar";
+import { IoArrowBackOutline } from "react-icons/io5";
 
 const AddList = () => {
+
+    document.title = "TourLand - Add Tourist Spots"
+
     return (
         <div>
             <Navbar></Navbar>
-            <h2>add list</h2>
+
+            <div className="mt-24">
+                <div className="flex gap-[400px]">
+                <div className="text-3xl"><IoArrowBackOutline /></div>
+                <h2 className="text-center text-2xl text-[#d7a31a] mb-10 bg-red-600">Add Tourist List</h2>
+                </div>
+            </div>
+
         </div>
     );
 };
