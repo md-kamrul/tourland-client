0b1d7d1ff4318b9a79b5a1d18828765b3e41293e
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-29 14:55:00 +0600

    SignleCard.jsx file done

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,15 +1,16 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { AiFillDollarCircle } from "react-icons/ai";
 import { IoTimeSharp } from "react-icons/io5";
+import { BsPeopleFill } from "react-icons/bs";
 
 const SingleCard = ({ signle_card }) => {
 
-    const { image, touristSpot, shortDescription, location, averageCost, travelTime } = signle_card;
+    const { image, touristSpot, shortDescription, location, averageCost, travelTime, totalVisitorPerYear } = signle_card;
 
     return (
         <div>
             <div className="card bg-[#4d95a7] border border-[#4d95a7] bg-opacity-30">
-                <figure className="px-10 pt-10">
+                <figure className="px-5 pt-5">
                     <img src={image} alt="Shoes" className="rounded-xl" />
                 </figure>
                 <div className="card-body items-center text-center">
@@ -33,6 +34,10 @@ const SingleCard = ({ signle_card }) => {
                             <IoTimeSharp className="text-xl text-[#4d95a7]" />
                             <p>{travelTime} Days</p>
                         </div>
+                    </div>
+                    <div className="flex items-center gap-2 mb-4">
+                        <BsPeopleFill className="text-xl text-[#4d95a7]" />
+                        <p>{totalVisitorPerYear} Person</p>
                     </div>
                     <div className="card-actions">
                         <button className="btn bg-[#4d95a7] text-[#000e25] border hover:border-[#4d95a7] border-[#4d95a7] hover:bg-opacity-50 hover:bg-[#4d95a7] hover:text-[#f8fbff]">View Details</button>
