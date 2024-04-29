1bf6d5ff25712876ff69163078744c3009092cf4
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-29 21:52:50 +0600

    average cost sorting button add on AllSpot.jsx file

--- src/components/AllSpot.jsx
+++ src/components/AllSpot.jsx
@@ -10,6 +10,16 @@ const AllSpot = () => {
         <div className="bg-[#000e25] text-[#f8fbff]">
             <Navbar></Navbar>
 
+            <div className="w-[80%] mx-auto mt-24 flex justify-center">
+                <div className="dropdown dropdown-bottom">
+                    <div tabIndex={0} role="button" className="btn m-1 bg-[#4d95a7] border border-[#4d95a7] text-[#f8fbff] hover:bg-[##f8fbff] hover:border-[#f8fbff] hover:text-[#4d95a7]">Average Cost</div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 bg-[#4d95a7] border border-[#4d95a7] text-[#f8fbff] hover:bg-[#f8fbff] hover:border-[#f8fbff] hover:text-[#4d95a7] rounded-box w-52">
+                        <li><a>Low to High</a></li>
+                        <li><a>High to Low</a></li>
+                    </ul>
+                </div>
+            </div>
+
             <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-24">
                 {
                     show_spot_list.map(signle_card => <SingleCard key={signle_card._id} signle_card={signle_card}></SingleCard>)
