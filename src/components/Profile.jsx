a01dcf0d40ec712664991e6c88adfd4878df1d08
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-30 13:08:04 +0600

    profile update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,53 @@
 import { useContext } from "react";
 import Navbar from "./Navbar";
 import { AuthContext } from "../firebase/AuthProvider";
+import Swal from 'sweetalert2'
+import app from "../firebase/firebase.config";
+import { getAuth } from "firebase/auth";
 
 const Profile = () => {
 
     document.title = "TourLand - Profile"
 
     const userInfo = useContext(AuthContext);
-    console.log(userInfo);
+    const auth = getAuth(app);
+    const user = auth.currentUser;
+
+    var displayName;
+    var email;
+
+    const updateProfile = e => {
+        e.preventDefault();
+
+        const userName = document.getElementById('userName').value;
+        const email = document.getElementById('email').value;
+
+        updateProfile(auth.currentUser, {
+            displayName: `${userName}`, email: `${email}`
+        }).then(() => {
+            // Profile updated!
+            Swal.fire({
+                title: "Done!",
+                text: `Account info has been updated...`,
+                icon: "success"
+            });
+        }).catch(() => {
+            // An error occurred
+            Swal.fire({
+                title: "Error!",
+                text: `Something went wrong. Please try again...`,
+                icon: "error"
+            });
+        });
+
+    }
+
+    if (user !== null) {
+        user.providerData.forEach((profile) => {
+            displayName = profile.displayName;
+            email = profile.email;
+        });
+    }
 
     return (
         <div className="bg-[#000e25] text-[#f8fbff]">
@@ -19,11 +59,11 @@ const Profile = () => {
 
                 <div className="mb-5">
                     <span className="text-[#4d95a7]">Name: </span>
-                    <input className="bg-[#000e25] p-2 rounded-md ml-3 pl-5 border border-[#4d95a7]" type="text" name="userName" defaultValue={userInfo.user.displayName} />
+                    <input className="bg-[#000e25] p-2 rounded-md ml-3 pl-5 border border-[#4d95a7]" type="text" id="userName" defaultValue={userInfo.user.displayName} />
                 </div>
                 <div className="mb-5">
                     <span className="text-[#4d95a7]">Email Address: </span>
-                    <input className="bg-[#000e25] p-2 rounded-md ml-3 pl-5 border border-[#4d95a7]" type="text" name="userName" defaultValue={userInfo.user.email} />
+                    <input className="bg-[#000e25] p-2 rounded-md ml-3 pl-5 border border-[#4d95a7]" type="email" id="email" defaultValue={userInfo.user.email} />
                 </div>
                 <div className="mb-5 flex gap-2 flex-col md:flex-row justify-center">
                     <span className="text-[#4d95a7]">Last Sign In: </span>
@@ -34,7 +74,7 @@ const Profile = () => {
                     <p>{userInfo.user.metadata.lastSignInTime}</p>
                 </div>
 
-                <button className="btn bg-[#4d95a7] border border-[#4d95a7] text-[#f8fbff] hover:bg-[##f8fbff] hover:border-[#f8fbff] hover:text-[#4d95a7] mt-5">Update Profile</button>
+                <button onClick={updateProfile} className="btn bg-[#4d95a7] border border-[#4d95a7] text-[#f8fbff] hover:bg-[##f8fbff] hover:border-[#f8fbff] hover:text-[#4d95a7] mt-5">Update Profile</button>
 
             </div>
         </div>
