bb03fd0325f113814018bc56eeddff7a155ab2d5
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-30 22:55:00 +0600

    add spin loader on all tourist spot section

--- src/components/AllSpot.jsx
+++ src/components/AllSpot.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import Navbar from "./Navbar";
 import SingleCard from "./SingleCard";
+import { useEffect, useState } from "react";
 
 const AllSpot = () => {
 
@@ -8,25 +9,44 @@ const AllSpot = () => {
 
     const show_spot_list = useLoaderData();
 
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+    }, [])
+
     return (
         <div className="bg-[#000e25] text-[#f8fbff]">
             <Navbar></Navbar>
 
-            <div className="w-[80%] mx-auto mt-24 flex justify-center">
-                <div className="dropdown dropdown-bottom">
-                    <div tabIndex={0} role="button" className="btn m-1 bg-[#4d95a7] border border-[#4d95a7] text-[#f8fbff] hover:bg-[##f8fbff] hover:border-[#f8fbff] hover:text-[#4d95a7]">Average Cost</div>
-                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 bg-[#4d95a7] border border-[#4d95a7] text-[#f8fbff] hover:bg-[#f8fbff] hover:border-[#f8fbff] hover:text-[#4d95a7] rounded-box w-52">
-                        <li><a>Low to High</a></li>
-                        <li><a>High to Low</a></li>
-                    </ul>
-                </div>
-            </div>
+            {
+                loading ?
+                    <div className="py-24 justify-center flex items-center">
+                        <div className="loading loading-dots loading-lg text-center"></div>
+                    </div>
 
-            <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-24">
-                {
-                    show_spot_list.map(signle_card => <SingleCard key={signle_card._id} signle_card={signle_card}></SingleCard>)
-                }
-            </div>
+                    :
+
+                    <div>
+                        <div className="w-[80%] mx-auto mt-24 flex justify-center">
+                            <div className="dropdown dropdown-bottom">
+                                <div tabIndex={0} role="button" className="btn m-1 bg-[#4d95a7] border border-[#4d95a7] text-[#f8fbff] hover:bg-[##f8fbff] hover:border-[#f8fbff] hover:text-[#4d95a7]">Average Cost</div>
+                                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 bg-[#4d95a7] border border-[#4d95a7] text-[#f8fbff] hover:bg-[#f8fbff] hover:border-[#f8fbff] hover:text-[#4d95a7] rounded-box w-52">
+                                    <li><a>Low to High</a></li>
+                                    <li><a>High to Low</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-24">
+                            {
+                                show_spot_list.map(signle_card => <SingleCard key={signle_card._id} signle_card={signle_card}></SingleCard>)
+                            }
+                        </div>
+                    </div>
+            }
 
         </div>
     );
